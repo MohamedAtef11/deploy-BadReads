bbb54ca94d746f59f43072e46d8754881522c8d5
MohamedAtef11 <nerophone2012@gmail.com>
2020-07-18 15:36:45 +0200

    project

---
+++ init-mongo.js
@@ -0,0 +1,28 @@
+ 
+db.createUser(
+    {
+        user: "admin",
+        pwd: "123456",
+        roles: [
+            {
+                role: "userAdminAnyDatabase",
+                db: "admin"
+
+            }
+        ]
+    }
+)
+
+db.createUser(
+    {
+        user: "root",
+        pwd: "123456",
+        roles: [
+            {
+                role: "root",
+                db: "admin"
+
+            }
+        ]
+    }
+)
